70cecf5fd255c27cc05fce92834f9c7732cc2481
muazsazelim <muazsazelim019@gmail.com>
2024-12-14 16:35:10 +0000

    add start javascript and install node

---
+++ foundations/javascript/javascript.js
@@ -0,0 +1,37 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+age = 54;
+ 
+console.log(age); // what will be output now?
+
+const pi = 3.14;
+console.log(pi); // What will be output?
+
+console.log((3 + 2) - 76 * (1 + 1))
+
+console.log(23 + 97)
+
+console.log((4 + 6 + 9) / 77)
+
+let a = 10
+console.log(a)
+console.log(9 * a)
+
+let b = 7 * a
+console.log(b)
+
+const max = 57
+
+let actual = max - 13
+
+let percentage = actual / max
+
+console.log(percentage)
+
